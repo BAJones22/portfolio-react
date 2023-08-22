8c924eb3e3db7ba3a55b23469bca4149d537e56d
Brooke Adlesperger-Jones <badles@live.com>
2023-08-21 21:41:13 -0500

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Home from './Pages/Home'
+import Navbar from './Components/Navbar'
+import Portfolio from './Pages/Portfolio'
+import Resume from './Pages/Resume'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [displayState, setDisplayState] = useState('')
 
+  const DisplaySwitch = (state) => {
+    switch (state) {
+      case '':
+        return <Home />
+      case 'about':
+        return <About />
+      case 'contact':
+        return <Contact />
+      case 'portfolio':
+        return <Portfolio />
+      case 'resume':
+        return <Resume />
+      default:
+        return <Home />
+    }
+  }
+  
   return (
     <>
-  
+  <Navbar setDisplayState={setDisplayState} />  
+  {DisplaySwitch(displayState)}
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-function Navbar() {
+function Navbar({setDisplayState}) {
   return (
     <div>
-      
+         <nav>
+            <img src="assets/images/logo.jpg" class="logo"></img>
+            <ul>
+                <li><button onClick={ () => setDisplayState('')}>Home</button></li>
+                <li><button onClick={ () => setDisplayState('about')}>About</button></li>
+                <li><button onClick={ () => setDisplayState('portfolio')}>Portfolio</button></li>
+                <li><button onClick={ () => setDisplayState('resume')}>Resume</button></li>
+                <li><button onClick={ () => setDisplayState('contact')}>Contact</button></li>
+            </ul>
+        </nav>
     </div>
   )
 }
